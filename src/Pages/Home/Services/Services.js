0457716595c00e326e2f230646631b49b6335398
ra-hul <rahulshanto008@gmail.com>
2022-08-03 01:28:14 +0600

    services done

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { experimentalStyled as styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import { Container } from "@mui/material";
+import Typography from "@mui/material/Typography";
 import Service from "../Service/Service";
 import fluoride from "../../../images/fluoride.png";
 import cavity from "../../../images/cavity.png";
@@ -33,15 +33,27 @@ const Services = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Container>
+        <Typography
+          sx={{ fontWeight: "500", m: 2, color: "success.main" }}
+          variant="h6"
+          component="div"
+        >
+          OUR Services
+        </Typography>
+        <Typography
+          sx={{ fontWeight: "600", m: 2 }}
+          variant="h4"
+          component="div"
+        >
+          Services we Provide
+        </Typography>
         <Grid
           container
           spacing={{ xs: 2, md: 3 }}
           columns={{ xs: 4, sm: 8, md: 12 }}
         >
-          {services.map((_, index) => (
-            <Grid item xs={4} sm={4} md={4} key={index}>
-              <Service></Service>
-            </Grid>
+          {services.map((service) => (
+            <Service key={service.name} service={service}></Service>
           ))}
         </Grid>
       </Container>
