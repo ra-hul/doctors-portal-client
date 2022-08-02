eb119a3dd2f1043cfa796c4f0f845b3404bab34a
ra-hul <rahulshanto008@gmail.com>
2022-08-02 23:36:15 +0600

    first commit

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { experimentalStyled as styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import { Container } from "@mui/material";
+import Service from "../Service/Service";
+import fluoride from "../../../images/fluoride.png";
+import cavity from "../../../images/cavity.png";
+import whitening from "../../../images/whitening.png";
+
+const services = [
+  {
+    name: "Fluoride Treatment",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed culpa cumque enim! Voluptatibus aliquid expedita saepe accusantium itaque ducimus rem voluptas",
+    img: fluoride,
+  },
+  {
+    name: "Cavity Filling",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed culpa cumque enim! Voluptatibus aliquid expedita saepe accusantium itaque ducimus rem voluptas",
+    img: cavity,
+  },
+  {
+    name: "Teeth Whitening",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed culpa cumque enim! Voluptatibus aliquid expedita saepe accusantium itaque ducimus rem voluptas",
+    img: whitening,
+  },
+];
+
+const Services = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Container>
+        <Grid
+          container
+          spacing={{ xs: 2, md: 3 }}
+          columns={{ xs: 4, sm: 8, md: 12 }}
+        >
+          {services.map((_, index) => (
+            <Grid item xs={4} sm={4} md={4} key={index}>
+              <Service></Service>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </Box>
+  );
+};
+
+export default Services;
