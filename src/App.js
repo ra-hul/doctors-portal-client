1e812a6373307052d0fc429868c57278d0c52830
ra-hul <rahulshanto008@gmail.com>
2022-08-08 15:02:10 +0600

    login section added

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home/Home";
 import Appointment from "./Pages/Appointment/Appointment/Appointment";
+import Login from "./Pages/Login/Login/Login";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/appointment" element={<Appointment />}>
-            {" "}
-          </Route>
+          <Route path="/appointment" element={<Appointment />}></Route>
+          <Route path="/login" element={<Login />}></Route>
           <Route path="/home" element={<Home />}></Route>
-          <Route path="/" element={<Home />}></Route>
+          <Route exact path="/" element={<Home />}></Route>
         </Routes>
       </Router>
     </div>
